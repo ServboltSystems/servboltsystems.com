18bda510ddb56cc489feea027fcbac668de67fb8
Servbolt Systems <servboltsystems@gmail.com>
2013-12-08 21:18:47 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ServboltsystemsCom::Application.config.secret_key_base = '095af7a7758b1a2a07a50c18990d99e875c68753f9f504b65b619049c77ff1f80b755d1c43e31f680e5637c1097750e3ea1c9c1175917a48fa516a6aec0c2115'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ServboltsystemsCom::Application.config.session_store :cookie_store, key: '_servboltsystems_com_session'
